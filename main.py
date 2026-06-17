5dbdc10fbf9ef83b774bffb878ed00bbf3a57777
Victor Sinaga <vee.raptord21@gmail.com>
2026-06-17 15:30:49 +0700

    Add Rankine conversion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 celsius = 25
 fahrenheit = (celsius*9/5)+32
 kelvin = celsius + 273.15
+rankine = (celsius + 273.15)*9/5
 
 print(f"{celsius} C = {fahrenheit} F")
 print(f"{celsius} C = {kelvin} K")
+print(f"{celsius} C = {rankine} R")
