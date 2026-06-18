6c9426881666695cf9a72996bf798546267f8a76
Victor Sinaga <95678967+victoraptord@users.noreply.github.com>
2026-06-18 14:51:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,3 @@ print(f"Temperature in Fahnrenheit: {fahrenheit}")
 
 print(f"{celsius} C = {kelvin} K")
 print(f"{celsius} C = {reamur} Re")
-
-delisle = (100 - celsius)*3/2
-print(f"{celsius} C = {delisle} De")
-
