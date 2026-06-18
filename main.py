60f8e26f211d30742383650aca5ddf4631d52a87
Your Name <your.email@example.com>
2026-06-18 15:04:33 +0700

    Add Sweet Soy Sauce

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ print(f"Temperature in Fahnrenheit: {fahrenheit}")
 print(f"{celsius} C = {kelvin} K")
 print(f"{celsius} C = {reamur} Re")
 
-print("Fried Rice")
+print("Sweet Soy Sauce Fried Rice")
 
