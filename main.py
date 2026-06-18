b43700f37e127ae77195cbaa044f67cd35664ff1
Victor Sinaga <vee.raptord21@gmail.com>
2026-06-18 10:43:24 +0700

    Add Reamur conversion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 celsius = 25
 fahrenheit = (celsius*9/5)+32
 kelvin = celsius + 273.15
+reamur = celsius*4/5
 
 print(f"{celsius} C = {fahrenheit} F")
 print(f"{celsius} C = {kelvin} K")
+print(f"{celsius} C = {reamur} Re")
