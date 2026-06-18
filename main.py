2c541ececced1863d65665de28e3b0ef2562fbdd
Victor Sinaga <vee.raptord21@gmail.com>
2026-06-18 11:07:34 +0700

    Improve Fahrenheit output text

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ fahrenheit = (celsius*9/5)+32
 kelvin = celsius + 273.15
 reamur = celsius*4/5
 
-print(f"{celsius} C = {fahrenheit} F")
+print(f"Temperature in Fahnrenheit = {fahrenheit}")
 print(f"{celsius} C = {kelvin} K")
 print(f"{celsius} C = {reamur} Re")
